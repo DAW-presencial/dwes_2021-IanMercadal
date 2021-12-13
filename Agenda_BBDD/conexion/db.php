8ed0325431066649b58aaff9eba2c493d9d182c2
Ian Mercadal <72464723+IanMercadal@users.noreply.github.com>
2021-12-13 20:40:44 +0100

    feat: :art: Insertando nuevo modo de conectarse

--- Agenda_BBDD/conexion/db.php
+++ Agenda_BBDD/conexion/db.php
@@ -4,7 +4,7 @@ class Database{
     // Credenciales
 
     // El usuario actualmente es el de Brais debido a que yo todavía no tengo
-    private $host = '51.178.152.213';
+    private $host = 'g1.ifc33b.cifpfbmoll.eu';
     private $db_name = 'bvirlan_db';
     private $username = 'bvirlan_usr';
     private $password = 'abc123.';
